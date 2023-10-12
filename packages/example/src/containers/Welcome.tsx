12f0f4cfc38994491ad8e9b28eb1bea9acd57975
Yann <yanjie@safeheron.com>
2023-10-12 19:59:47 +0800

    style: change the padding and refactor styles

--- packages/example/src/containers/Welcome.tsx
+++ packages/example/src/containers/Welcome.tsx
@@ -1,15 +1,26 @@
+import styled from 'styled-components'
+
 import welcome from '@/assets/welcome.png'
 import InstallReminder from '@/components/InstallReminder'
-import styles from '@/styles/containers/Welcome.module.less'
+
+const WelcomeContainer = styled.div`
+  padding: 100px 0 110px;
+
+  > .img-box {
+    margin-top: 88px;
+    padding: 0 80px;
+    text-align: center;
+  }
+`
 
 const Welcome = () => {
   return (
-    <div className={styles.welcome}>
+    <WelcomeContainer>
       <InstallReminder />
-      <div className={styles.imgBox}>
+      <div className={'img-box'}>
         <img src={welcome} width="869" />
       </div>
-    </div>
+    </WelcomeContainer>
   )
 }
 
