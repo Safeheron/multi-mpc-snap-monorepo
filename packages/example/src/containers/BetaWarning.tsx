45f8b3d25a6a91d59120afa03d588ed41def490c
Yann <yanjie@safeheron.com>
2023-10-31 14:17:41 +0800

    chore: change style of BetaWarning

--- packages/example/src/containers/BetaWarning.tsx
+++ packages/example/src/containers/BetaWarning.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 
 const BetaWrap = styled.div`
   width: 100%;
-  height: 40px;
-  line-height: 40px;
+  padding: 16px 0;
   background-color: #e972071a;
   text-align: center;
   color: #e97207;
