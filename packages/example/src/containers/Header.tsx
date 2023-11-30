b6d90ad2b316f470a346a05897ae1f1ac4a10095
Yann <yanjie@safeheron.com>
2023-11-30 18:34:41 +0800

    chore: change network indicator style

--- packages/example/src/containers/Header.tsx
+++ packages/example/src/containers/Header.tsx
@@ -1,13 +1,69 @@
 import { observer } from 'mobx-react-lite'
 import { FC, PropsWithChildren } from 'react'
+import styled from 'styled-components'
 
 import logo from '@/assets/logo.png'
 import { useStore } from '@/store'
-import styles from '@/styles/containers/Header.module.less'
 
 interface HeaderProps extends PropsWithChildren {
   hideNetwork?: boolean
 }
+
+const HeaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 9;
+  backdrop-filter: blur(5px);
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 73px;
+    margin: 0 auto;
+    width: 1200px;
+    .logo-box {
+      flex: 1;
+      display: flex;
+      cursor: pointer;
+      .text {
+        margin-left: 12px;
+      }
+      .title {
+        font-size: 20px;
+        color: #262833;
+        font-weight: bold;
+        margin-bottom: 4px;
+        margin-top: 3px;
+      }
+      .desc {
+        margin-top: -5px;
+        font-size: 12px;
+        color: #6b6d7c;
+      }
+    }
+    .network {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      height: 73px;
+      align-items: center;
+      span {
+        height: 32px;
+        line-height: 32px;
+        padding: 0 20px;
+        border-radius: 32px;
+        border: 1px dashed #d9d9d9;
+      }
+    }
+    .right-content {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+`
 
 const Header: FC<HeaderProps> = ({ hideNetwork, children }) => {
   const { networkModule } = useStore()
@@ -18,30 +74,30 @@ const Header: FC<HeaderProps> = ({ hideNetwork, children }) => {
   }
 
   return (
-    <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.logoBox} onClick={toIndex}>
+    <HeaderWrapper>
+      <div className={'container'}>
+        <div className={'logo-box'} onClick={toIndex}>
           <div>
             <img src={logo} width={42} alt={'Safeheron Logo'} />
           </div>
-          <div className={styles.text}>
-            <div className={styles.title}>Safeheron Snap</div>
-            <div className={styles.desc}>Enable MPC Wallet within MetaMask</div>
+          <div className={'text'}>
+            <div className={'title'}>Safeheron Snap</div>
+            <div className={'desc'}>Enable MPC Wallet within MetaMask</div>
           </div>
         </div>
         {hideNetwork ? (
           <div></div>
         ) : (
-          <div className={styles.network}>
+          <div className={'network'}>
             {fetchChainListFailed && (
               <span>ChainId: {parseInt(hexChainId)}</span>
             )}
             {chainName && <span>{chainName}</span>}
           </div>
         )}
-        <div className={styles.rightContent}>{children}</div>
+        <div className={'right-content'}>{children}</div>
       </div>
-    </div>
+    </HeaderWrapper>
   )
 }
 
