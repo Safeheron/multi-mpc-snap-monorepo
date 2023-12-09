cd443ef45c44ff094110400314ab619480f28e03
Yann <yanjie@safeheron.com>
2023-12-09 11:13:12 +0800

    style: refactor styles

--- packages/example/src/components/Loading.tsx
+++ packages/example/src/components/Loading.tsx
@@ -1,21 +1,41 @@
 import { Loading3QuartersOutlined } from '@ant-design/icons'
 import { FC } from 'react'
+import styled from 'styled-components'
 
-import styles from '@/styles/components/Loading.module.less'
 interface Props {
   loading: boolean
   text?: string
 }
+
+const LoadingContainer = styled.div`
+  position: fixed;
+
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  z-index: 9999;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  span {
+    margin-bottom: 10px;
+    color: white;
+  }
+`
 
 const Loading: FC<Props> = ({ loading, text }) => {
   if (!loading) {
     return null
   }
   return (
-    <div className={styles.loading}>
+    <LoadingContainer>
       <span>{text}</span>
       <Loading3QuartersOutlined style={{ fontSize: 42, color: 'white' }} spin />
-    </div>
+    </LoadingContainer>
   )
 }
 
